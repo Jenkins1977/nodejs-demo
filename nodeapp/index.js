12aaa3e0828f08b5e0559f8829a62be44c51f671
Rahul Tewari3 <rtewari3@in.ibm.com>
2022-08-13 13:53:04 +0530

    k8

--- nodeapp/index.js
+++ nodeapp/index.js
@@ -2,14 +2,14 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello, Welcome to Valaxy" }');
+    res.send('{ "response": "Hello, Welcome to Rahul Tewari "/s web page" }');
 });
 
 app.get('/will', function (req, res) {
-    res.send('{ "response": "Hello World" }');
+    res.send('{ "response": "Hello World at my website" }');
 });
 app.get('/ready', function (req, res) {
-    res.send('{ "response": " Great!, It works!" }');
+    res.send('{ "response": " Great!, It works! Wow!!" }');
 });
 app.listen(process.env.PORT || 3000);
 module.exports = app;
